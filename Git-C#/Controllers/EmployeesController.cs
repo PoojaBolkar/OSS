705026024a7110b691e893db58f54d5f230a145a
PoojaBolkar <108930444+PoojaBolkar@users.noreply.github.com>
2022-07-08 17:01:10 +0530

    New Data Add

---
+++ Git-C#/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using HRWebApplication.Models;
+namespace HRWebApplication.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+            List<Employee> list = HRManager.getData();
+            return View(list);
+        }
+        public ActionResult Create()
+        {
+            List<Employee> list = HRManager.getData();
+            return View(list);
+        }
+    }
+}
